4f84e0627b473229d2227c7296e9bfae3422aae6
jentlAntheunis <113892951+jentlAntheunis@users.noreply.github.com>
2023-09-27 15:41:20 +0200

    labo done, toto: dark mode

--- script/app.js
+++ script/app.js
@@ -1,14 +1,28 @@
 const handlePasswordSwitcher = function () {
-    console.log("handlePasswordSwitcher executed");
-}
+  //select the password switcher
+  const passwordSwitcher = document.querySelector('.js-password-toggle-checkbox');
+  //select the password field
+  const passwordField = document.querySelector('.js-password-input');
+  //listen to clicking the switcher
+  passwordSwitcher.addEventListener('click', (e) => {
+    //check type of password field
+    const fieldType = passwordField.type;
+    //if password, make text and vice versa
+    if (fieldType == 'password') {
+      document.querySelector('.js-password-input').type = 'text';
+    } else if (fieldType == 'text') {
+      document.querySelector('.js-password-input').type = 'password';
+    }
+  });
+};
 
 const init = function () {
-    console.log('Script loaded!');
+  console.log('Script loaded!');
 
-    // init password switcher
-    handlePasswordSwitcher();
+  // init password switcher
+  handlePasswordSwitcher();
 
-    // Other init functions here
-}
+  // Other init functions here
+};
 
-init();
+document.addEventListener('DOMContentLoaded', init);
